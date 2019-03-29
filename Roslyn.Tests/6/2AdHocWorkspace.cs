5d14afa5fe9bd8002f80556a7a9c91336d2c59df
Radek Jančík <radek.jancik@sunamo.cz>
2019-03-30 00:10:42 +0100

    Mass commit 30-3-2019

--- Roslyn.Tests/6/2AdHocWorkspace.cs
+++ Roslyn.Tests/6/2AdHocWorkspace.cs
@@ -1,40 +1,46 @@
-﻿//using Microsoft.CodeAnalysis;
-//using Microsoft.CodeAnalysis.CSharp.Syntax;
-//using System.Collections.Generic;
-//using System;using Xunit;
-//using Microsoft.CodeAnalysis;
-//using Microsoft.CodeAnalysis.CSharp.Syntax;
-//using System.Collections.Generic;
-//using System;using Xunit;
-//using Microsoft.Build.Evaluation;
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections.Generic;
+using System;
+using Xunit;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections.Generic;
+using System;
+using Xunit;
+using Microsoft.Build.Evaluation;
+using Microsoft.CodeAnalysis.Text;
 
-//public partial class RoslynLearn
-//{
-//    [Fact]
-//public void _2AdHocWorkspace()
-//    {
-//        // api for add and remove files is different within AdhocWorkspace  and other
-//        var workspace = new AdhocWorkspace();
-        
-//        string projName = "NewProject";
-//        // Create new project
-//        var projectId = Project.CreateNewId();
-//        var versionStamp = VersionStamp.Create();
-//        var projectInfo = ProjectInfo.Create(projectId, versionStamp, projName, projName, LanguageNames.CSharp);
-        
-//        // add into project
-//        var newProject = workspace.AddProject(projectInfo);
-        
-//        var sourceText = SourceText.From("class A {}");
-//        var newDocument = workspace.AddDocument(newProject.Id, "NewFile.cs", sourceText);
-        
-//        foreach (var project in workspace.CurrentSolution.Projects)
-//        {
-//        	foreach (var document in project.Documents)
-//        	{
-//        		DebugLogger.Instance.WriteLine(project.Name + "\t\t\t" + document.Name);
-//        	}
-//        }
+public partial class RoslynLearn
+{
+    [Fact]
+    public void _2AdHocWorkspace()
+    {
+        // api for add and remove files is different within AdhocWorkspace  and other
+        var workspace = new AdhocWorkspace();
 
-//    }
-//}
+        string projName = "NewProject";
+        // Create new project - 
+        ProjectId projectId = null;
+        // CreateNewId() should be in https://docs.microsoft.com/en-us/dotnet/api/microsoft.codeanalysis.projectid.createnewid?view=roslyn-dotnet
+        //projectId = Microsoft.CodeAnalysis.Project.CreateNewId();
+
+        var versionStamp = VersionStamp.Create();
+        var projectInfo = ProjectInfo.Create(projectId, versionStamp, projName, projName, LanguageNames.CSharp);
+
+        // add into project
+        var newProject = workspace.AddProject(projectInfo);
+
+        var sourceText = SourceText.From("class A {}");
+        var newDocument = workspace.AddDocument(newProject.Id, "NewFile.cs", sourceText);
+
+        foreach (var project in workspace.CurrentSolution.Projects)
+        {
+            foreach (var document in project.Documents)
+            {
+                DebugLogger.Instance.WriteLine(project.Name + "\t\t\t" + document.Name);
+            }
+        }
+
+    }
+}
