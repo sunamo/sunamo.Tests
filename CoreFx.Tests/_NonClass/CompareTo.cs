50f4399c8bace776e046cfbbcb47142061b931d0
Radek Jančík <radek.jancik@sunamo.cz>
2019-04-16 00:34:48 +0200

    Mass commit 16-4-2019

---
+++ CoreFx.Tests/_NonClass/CompareTo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/// <summary>
+/// Generic number can be compare in this way
+/// </summary>
+public class CompareTo
+{
+    void TestCompareTo()
+    {
+        var a = 1;
+        var b = 2;
+        var i = a.CompareTo(b);
+        // A is lower, return -1
+        Console.WriteLine(i);
+    }
+}
+
