832d9a0e0397bb8866340ab2b0575602cd8e5560
Radek Jančík <radek.jancik@sunamo.cz>
2019-04-23 00:54:00 +0200

    Mass commit 23-4-2019

--- sunamo.Tests/Storage/ApplicationDataTextTests.cs
+++ sunamo.Tests/Storage/ApplicationDataTextTests.cs
@@ -1,15 +1,25 @@
-﻿using System;
+﻿using sunamo.Storage;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
 
 namespace sunamo.Tests.Storage
 {
     public class ApplicationDataTextTests
     {
+        [Fact]
         public void ParseTest()
         {
-            string test = @"D:\_Test\ConsoleApp1\ConsoleApp1\ApplicationDataText.txt";
-            
+            string testFile = @"D:\_Test\ConsoleApp1\ConsoleApp1\ApplicationDataText.txt";
+
+            var headers = CA.ToListString("Copy", "Dont copy to");
+            CA.AddSuffix(headers, ":");
+
+            var value1 = CA.ToListString("Shared");
+
+            var dict = ApplicationDataText.Parse(testFile, headers);
+            Assert.Equal<string>(value1, dict[headers[0]]);
         }
     }
 }
