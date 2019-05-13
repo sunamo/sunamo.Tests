821f3c77a3d37dabfd8684d3dbc00c6dae68abef
Radek Jančík <radek.jancik@sunamo.cz>
2019-05-13 22:56:03 +0200

    before replace internal for public

---
+++ SunamoCode.Tests/FileFormats/XmlLocalisationInterchangeFileFormatTests.cs
@@ -0,0 +1,16 @@
+﻿using SunamoCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+public class XmlLocalisationInterchangeFileFormatTests
+{
+    [Fact]
+    public void AppendTest()
+    {
+        var file = @"d:\_Test\sunamo\shared\ParseAndRemoveNamespacesTest\a.xlf";
+        XmlLocalisationInterchangeFileFormat.Append(Langs.cs, "Hello", "Ahoj", "HelloID", file);
+    }
+}
+
---
+++ shared.Tests/Helpers/XmlHelperTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using Xunit;
+
+public class XmlHelperTests
+{
+    [Fact]
+    public void ParseAndRemoveNamespacesTest()
+    {
+        var file = @"d:\_Test\sunamo\shared\ParseAndRemoveNamespacesTest\a.xlf";
+        var c = TF.ReadFile(file);
+        XmlNamespacesHolder h = new XmlNamespacesHolder();
+
+        XmlDocument x = null;
+
+        x = h.ParseAndRemoveNamespaces(c, x.NameTable);
+    }
+}
+
