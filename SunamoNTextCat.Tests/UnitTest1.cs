48b6cd55c165ac741abaffc2d8ccb22d44025d7c
Radek Jančík <radek.jancik@sunamo.cz>
2019-05-22 10:39:39 +0200

    Mass commit 22-5-2019

---
+++ SunamoNTextCat.Tests/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xunit;
+
+namespace SunamoNTextCat.Tests
+{
+    public class UnitTest1
+    {
+        const string cs = "ahoj světe";
+        const string en = "hello world";
+
+        [Fact]
+        public void IsCzechTest()
+        {
+            var t = TextLang.IsCzech(cs);
+            var f = TextLang.IsCzech(en);
+
+            Assert.True(t);
+            Assert.False(f);
+        }
+
+        [Fact]
+        public void IsEnglishTest()
+        {
+            var t = TextLang.IsEnglish(en);
+            var f = TextLang.IsEnglish(cs);
+
+            Assert.True(t);
+            Assert.False(f);
+        }
+    }
+}
