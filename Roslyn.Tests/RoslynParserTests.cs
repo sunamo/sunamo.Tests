eb94ce1b71c8f9e02b056f2c1329c188e18abca4
Radek Jančík <radek.jancik@sunamo.cz>
2019-06-27 11:59:11 +0200

    Mass commit 27-6-2019

---
+++ Roslyn.Tests/RoslynParserTests.cs
@@ -0,0 +1,24 @@
+﻿using Roslyn;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+public class RoslynParserTests
+{
+    [Fact]
+    /// <summary>
+    /// 
+    /// </summary>
+    public static void IsCSharpCodeTest()
+    {
+        var input1 = "abc";
+        var input2 = @"using System;
+
+using System.Collections.Generic;
+";
+        Assert.False(RoslynParser.IsCSharpCode(input1));
+        Assert.True(RoslynParser.IsCSharpCode(input2));
+    }
+}
+
---
+++ win.Tests/Helpers/Powershell/PowershellHelperTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using win.Helpers.Powershell;
+
+[TestClass]
+public class PowershellHelperTests
+{
+    [TestMethod]
+    public void DetectLanguageForFileGithubLinguistTest()
+    {
+        const string file = @"d:\_Test\sunamo\win\Helpers\Powershell\PowershellHelper\Program.cs";
+        var expected = "C#";
+
+        var actual = PowershellHelper.DetectLanguageForFileGithubLinguist(file);
+        Assert.AreEqual(expected, actual);
+
+    }
+}
+
