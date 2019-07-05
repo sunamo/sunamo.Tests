981abe7dd5ea941f3daa59fe12f76eb5cb6aea1d
Radek Jančík <radek.jancik@sunamo.cz>
2019-07-05 04:18:35 +0200

    Mass commit 5-7-2019

---
+++ CoreFx.Tests/SystemTests/ConvertTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+public class ConvertTests
+{
+    [Fact]
+    public void ToInt32Test()
+    {
+        var f= Convert.ToInt32(false);
+        var t = Convert.ToInt32(true);
+
+        int i = 0;
+    }
+}
+
