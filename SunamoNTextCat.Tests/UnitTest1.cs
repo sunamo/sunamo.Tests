b11234213763cb7a035bfa3c97418e0766b34862
Radek Jančík <radek.jancik@sunamo.cz>
2019-07-29 01:47:10 +0200

    Mass commit 29-7-2019

--- SunamoNTextCat.Tests/UnitTest1.cs
+++ SunamoNTextCat.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using Xunit;
 
 namespace SunamoNTextCat.Tests
@@ -8,33 +9,53 @@ namespace SunamoNTextCat.Tests
         const string cs = "ahoj světe";
         const string en = "hello world";
         const string cs2 = "error: Špatné údaje o přihlášení - odhlašte se a přihlašte se znovu.";
+        const string en2 = "error: invalid data to login - please log out and sign in again";
 
         [Fact]
         public void IsCzechTest()
         {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
             TextLang.Init();
 
             var t = TextLang.IsCzech(cs);
             var t2 = TextLang.IsCzech(cs2);
             var f = TextLang.IsCzech(en);
+            var f2 = TextLang.IsCzech(en2);
 
             Assert.True(t);
             Assert.True(t2);
             Assert.False(f);
+            Assert.False(f2);
+
+            sw.Stop();
+
+            DebugLogger.Instance.WriteLine("Is czech: " + sw.ElapsedMilliseconds);
+
         }
 
         [Fact]
         public void IsEnglishTest()
         {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
             TextLang.Init();
 
             var t = TextLang.IsEnglish(en);
+            var t2 = TextLang.IsEnglish(en2);
             var f = TextLang.IsEnglish(cs);
             var f2 = TextLang.IsEnglish(cs2);
 
             Assert.True(t);
+            Assert.True(t2);
             Assert.False(f);
             Assert.False(f2);
+
+            sw.Stop();
+
+            DebugLogger.Instance.WriteLine("Is english: " + sw.ElapsedMilliseconds);
         }
     }
 }
---
+++ sunamo.Tests/Helpers/i18n/AppLangHelperTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using sunamo;
+using Xunit;
+
+public class AppLangHelperTests
+{
+    [Fact]
+    public void GetLang3Test()
+    {
+        Langs actual = Langs.cs;
+
+        string input = "cs-CZ";
+        Langs expected = Langs.cs;
+
+        actual = AppLangHelper.GetLang3(input);
+        Assert.Equal(expected, actual);
+
+        input = "en-us";
+        expected = Langs.en;
+
+        actual = AppLangHelper.GetLang3(input);
+        Assert.Equal(expected, actual);
+    }
+}
+
