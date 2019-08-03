80bd0b2a3dbb2fd0f68226552d93de5ac45bc09c
Radek Jančík <radek.jancik@sunamo.cz>
2019-08-03 17:11:51 +0200

    Mass commit 3-8-2019

---
+++ Roslyn.Tests/RoslynHelperTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using Roslyn;
+using Xunit;
+
+public class RoslynHelperTests
+{
+    /// <summary>
+    /// better is use d:\pa\CodeFormatter\ with lang attr
+    /// </summary>
+    [Fact]
+    public void FormatTest()
+    {
+        string input = @"    public partial class InsertIntoXlfAndConstantCsUC : UserControl, IUserControl, IKeysHandler<KeyEventArgs>, IUserControlWithSettingsManager, IUserControlWithMenuItemsList, IWindowOpener, IUserControlWithSizeChange
+    {
+        #region Class data
+        static InsertIntoXlfAndConstantCsUC instance = null;
+#endregion
+}";
+
+        var s = RoslynHelper.Format(input);
+        Debug.WriteLine(s);
+    }
+}
+
