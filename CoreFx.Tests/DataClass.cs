7025fbd1bad748b683b97f16a8af51107f00671c
Radek Jančík <radek.jancik@sunamo.cz>
2019-08-08 00:30:27 +0200

    Mass commit 8-8-2019

---
+++ CoreFx.Tests/DataClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class DataClass
+{
+    public string variable = null;
+
+    public string Property
+    {
+        get
+        {
+            return variable + "2";
+        }
+    }
+}
+
---
+++ CoreFx.Tests/SystemTests/TypeTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+public class TypeTests
+{
+    [Fact]
+    public void GetPropertyAndVariables()
+    {
+        var dc = new DataClass() { variable = "a" };
+        dc.variable = "a";
+
+        var d = RH.GetValueOfPropertyOrField(dc, "variable");
+        var s = RH.GetValueOfPropertyOrField(dc, "Property");
+
+        int i = 0;
+    }
+}
+
