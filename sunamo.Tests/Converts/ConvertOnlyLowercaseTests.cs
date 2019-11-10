27318a475fbac49f9c7d49ff25cda59ddf88321a
Radek Jančík <sunamocz@outlook.com>
2019-11-10 21:53:16 +0100

    Mass commit 10-11-2019

---
+++ sunamo.Tests/Converts/ConvertOnlyLowercaseTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+public class ConvertOnlyLowercaseTests
+{
+    [Fact]
+    public void ToTest()
+    {
+        var input = "nY-aVdc_qu4";
+        var expected = "n*y-a*vdc_qu4";
+
+
+        var s = ConvertOnlyLowercase.To(input);
+        Assert.Equal(expected, s);
+    }
+
+    [Fact]
+    public void FromTest2()
+    {
+        var input = "n*y-a*vdc_qu4";
+        var expected = "nY-aVdc_qu4";
+
+        var s = ConvertOnlyLowercase.From(input);
+        Assert.Equal(expected, s);
+    }
+
+}
+
