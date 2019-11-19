8eea69031ca403dbb1f434192a864851e7380aa5
Radek Jančík <sunamocz@outlook.com>
2019-11-19 11:02:11 +0100

    Mass commit 19-11-2019

---
+++ desktop.Tests/VisualTreeHelpersTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+[TestClass]
+public class VisualTreeHelpersTests
+{
+    [TestMethod]
+    public void FindDescendentsTest()
+    {
+        var sp = new StackPanel();
+        var chb = new CheckBox();
+        var tb = new TextBlock();
+
+        const string inputText = "Hello world!";
+        tb.Text = inputText;
+
+        chb.Content = tb;
+        sp.Children.Add(chb);
+
+        var ele = VisualTreeHelpers.FindDescendents<TextBlock>(sp);
+        var tb2 = ele.First();
+        Assert.AreEqual(inputText, tb2.Text);
+    }
+}
+
