164802d10db8f906030efcc8b752f321bfe0550b
Radek Jančík <sunamocz@outlook.com>
2020-01-27 02:10:06 +0100

    Mass commit 27-1-2020

---
+++ CoreFx.Tests/DefaultTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit;
+
+namespace CoreFx.Tests.SystemTests
+{
+    /// <summary>
+    /// https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/default-values
+    /// </summary>
+    public class DefaultTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
