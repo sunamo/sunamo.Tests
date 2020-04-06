a629afdbf7c9b50b610a665cd93329460ec58647
Radek Jancik <ed@jepsano.net>
2020-04-06 12:00:21 +0200

    Mass commit 6-4-2020

---
+++ sunamo.Tests/Entity/ManageArtistDashTitleTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+public class ManageArtistDashTitleTests
+{
+    [Fact]
+    public void GetArtistTitleRemix()
+    {
+        
+    }
+}
