1f05c6ba45d3dba5423fe0d3411de033af75e1c6
Radek Jančík <radek.jancik@sunamo.cz>
2020-08-02 14:27:05 +0200

    Mass commit 2-8-2020

--- sunamo.Tests/Converts/ConvertPascalConventionTests.cs
+++ sunamo.Tests/Converts/ConvertPascalConventionTests.cs
@@ -11,9 +11,22 @@ public class ConvertPascalConventionTests
         var input = @"HelloWorld";
         var input2 = @"HelloWorld";
         var input3 = @"Hello World";
+        
 
         Assert.Equal(true, ConvertPascalConvention.IsPascal(input));
         Assert.Equal(false, ConvertPascalConvention.IsPascal(input2));
         Assert.Equal(false, ConvertPascalConvention.IsPascal(input3));
+        
+    }
+
+    [Fact]
+    public void ToConventionTest()
+    {
+        var input = @"Custom Field 2 - Value";
+        var expected = "CustomField2Value";
+
+        var tc = ConvertPascalConventionWithNumbers.ToConvention(input);
+
+        Assert.Equal(expected, tc);
     }
 }
