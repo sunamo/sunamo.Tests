5f5c7d45c690f69315078c5b76231c0c8147953b
Radek Jančík <radek.jancik@sunamo.cz>
2020-08-22 18:51:16 +0200

    Mass commit 22-8-2020

---
+++ desktop.Tests/Helpers/Content/BitmapHelperTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[TestClass]
+public class BitmapHelperTests
+{
+    string GetFile(string n)
+    {
+        return @"d:\_Test\sunamo\desktop\Helpers\Controls\BitmapHelperTests\"+n+".png";
+    }
+
+    [TestMethod]
+    public void ChangeColor2Test()
+    {
+        Bitmap bmp = new Bitmap(GetFile("In"));
+        var nB = BitmapHelper.ChangeColor2(bmp, Color.Black, Color.Orange);
+        nB.Save(GetFile("Out"));
+    }
+}
---
+++ sunamo.Tests/Helpers/Types/EnumHelperTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+public class EnumHelperTests
+{
+    [Fact]
+    public void ParseFromNumberTest()
+    {
+        var actual = EnumHelper.ParseFromNumber<Browsers, byte>(6, Browsers.Chrome);
+    }
+}
