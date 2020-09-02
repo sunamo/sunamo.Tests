fb16fc370bb72c3559793042781cfd9ca9673b33
Radek Jančík <radek.jancik@sunamo.cz>
2020-09-02 22:59:38 +0200

    Mass commit 2-9-2020

---
+++ desktop.Tests/Helpers/Content/Resource/BitmapImageHelperTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+[TestClass]
+public class BitmapImageHelperTests
+{
+    string GetFile(string n)
+    {
+        return @"d:\_Test\sunamo\desktop\Helpers\Content\Resource\BitmapImageHelper\Bitmap2BitmapImage\" + n +".png";
+    }
+
+    [TestMethod]
+    public void Bitmap2BitmapImageTest()
+    {
+        var bitmap = new Bitmap(GetFile("17"));
+        bitmap = BitmapHelper.ChangeColor2(bitmap, System.Drawing.Color.Black, ColorH.RandomColor(false).ToSystemDrawing());
+
+        BitmapImage biVsLogo = BitmapImageHelper.Bitmap2BitmapImage(bitmap);
+
+        var outFile = GetFile("out");
+        BitmapImageHelper.Save(biVsLogo, outFile);
+        PH.Start(outFile);
+    }
+}
---
+++ sunamo.Tests/Html/HtmlHelperSunamoCzTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+public class HtmlHelperSunamoCzTests
+{
+    /*
+
+     */
+
+    [Fact]
+    public void ConvertTextToHtmlWithAnchorsTest()
+    {
+        //Užitečný nástroj který stahuje auta ze 
+        var input = @"https://sauto.cz a vytváří tabulku v Google Sheets pro snadné porovnání dostupných dat pro zvolení auta jež je nejblíže Vašemu snu o nové mobilitě.";
+        var actual = HtmlHelperSunamoCz.ConvertTextToHtmlWithAnchors(input);
+        int s = 0;
+    }
+}
