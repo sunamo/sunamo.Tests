19ce6b8e2bdface1f188e4da03d34b5d2361df66
Radek Jančík <sunamocz@outlook.com>
2021-01-19 23:13:43 +0100

    Mass commit 19-1-2021

---
+++ win.Tests/WindowsOSHelperTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[TestClass]
+public class WindowsOSHelperTests
+{
+    [TestMethod]
+    public void FileInTest()
+    {
+        var p = PHWin.AddBrowser(Browsers.Seznam);
+        Assert.Equals(p, @"C:\Users\j\AppData\Roaming\Seznam Browser\Seznam.cz.exe");
+    }
+}
