a8f6bd98aadb2b610a507f79e667cab3cf8fa054
Radek Jančík <sunamocz@outlook.com>
2021-03-18 23:49:46 +0100

    Mass commit 18-3-2021

---
+++ sunamo.Tests/Helpers/Number/NormalizeDateTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+public class NormalizeDateTests
+{
+    [Fact]
+    public void NormalizeDateTest()
+    {
+        var dt = new DateTime(2021, 1, 1);
+
+        for (int i = 0; i < 12; i++)
+        {
+            
+            var d = NormalizeDate.To(dt);
+            var s = NormalizeDate.From(d);
+
+            Assert.Equal<DateTime>(dt, s);
+
+            dt = dt.AddMonths(1);
+        }
+    }
+}
