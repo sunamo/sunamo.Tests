1a0bf9fa6f14f7f204bcca3dd8fc5573e6f296f0
Radek Jančík <sunamocz@outlook.com>
2021-04-08 23:51:17 +0200

    Mass commit 8-4-2021

---
+++ win.Tests/Helpers/MimeHelperTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using sunamo.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[TestClass]
+public class MimeHelperTests
+{
+    static Type type = typeof(MimeHelperTests);
+
+    [TestMethod]
+    public void GetMimeFromFileTest()
+    {
+        var f = @"d:\_Test\sunamo\win\Helpers\MImeHelper\GetMimeFromFile\Real";
+        //application/octet-stream>
+        Assert.AreEqual(string.Empty, MimeHelper.GetMimeFromFile(f + AllExtensions.webp));
+        // 
+        Assert.AreEqual(string.Empty, MimeHelper.GetMimeFromFile(f + AllExtensions.jpg));
+    }
+}
