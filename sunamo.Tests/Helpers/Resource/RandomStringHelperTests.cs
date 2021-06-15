f13e792f5b5ae4b928476a1e830b5f8ee086d81f
sunamo <radek.jancik@sunamo.cz>
2021-06-15 13:48:15 +0200

    From Local

---
+++ sunamo.Tests/Helpers/Resource/RandomStringHelperTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+public class RandomStringHelperTests
+{
+    [Fact]
+    public void RandomStringTest()
+    {
+        var v = RandomStringHelper.RandomString(4, 2);
+        var v2 = RandomStringHelper.RandomString(4, 1);
+        int i = 0;
+    }
+}
